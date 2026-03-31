9463801178722f173ab08c025e1d420b22fbeddf
TI-nando <fernandoagro.654@gmail.com>
2026-03-31 10:20:08 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaController.java
@@ -10,4 +10,40 @@ public class NinjaController {
         return "Essa e a minha primeira mensagem nessa rota.";
     }
 
+            // ----- CRUD -----
+
+     // Adicionar ninja (CREATE)
+
+    @PostMapping("/criar")
+    public String criarNInja() {
+        return "Ninja Criado";
+    }
+
+    // Mostrar ninja por ID (CREATE)
+
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninjas";
+    }
+
+    // Mostrar todos os ninjas (READ)
+
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasID() {
+        return "Mostrar Ninjas ID";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja ID";
+    }
+
+    // Deletar ninja (DELETE)
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja Deletado Por Id";
+    }
 }
